006f036ab491ca1501baa0f2ef0687983f8e6cc3
Mamat1411 <muhammadmujahid14@gmail.com>
2024-02-05 10:34:09 +0700

    Initial Commit

---
+++ src/main/java/com/springboot/springblogmvc/SpringblogmvcApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springblogmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringblogmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringblogmvcApplication.class, args);
+	}
+
+}
