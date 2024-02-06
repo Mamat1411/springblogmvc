c184a02707db064a34a061c1f5feaa89705db96b
Mamat1411 <muhammadmujahid14@gmail.com>
2024-02-06 14:55:17 +0700

    Making Database Seeding Logic

--- src/main/java/com/springboot/springblogmvc/SpringblogmvcApplication.java
+++ src/main/java/com/springboot/springblogmvc/SpringblogmvcApplication.java
@@ -1,13 +1,43 @@
 package com.springboot.springblogmvc;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.github.javafaker.Faker;
+import com.springboot.springblogmvc.entities.User;
+import com.springboot.springblogmvc.repositories.UserRepository;
 
 @SpringBootApplication
 public class SpringblogmvcApplication {
+
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringblogmvcApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			int max = 10;
+			Faker faker = new Faker(new Locale("en-US"));
+
+			BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+			User user = new User("Muhammad Mujahid", "mamat1411", "mamat1411@gmail.com", bcrypt.encode("mamat1411"));
+			userRepository.save(user);
+			for (int i = 0; i < max; i++) {
+				User userSeed = new User(faker.name().fullName(), faker.name().username(), faker.internet().safeEmailAddress(), bcrypt.encode("123456"));
+				userRepository.save(userSeed);
+			}
+		};
+	}
 }
