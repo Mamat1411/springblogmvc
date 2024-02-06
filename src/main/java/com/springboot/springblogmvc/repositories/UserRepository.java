8661a015cb7c9821b6d1ef8454d708d4708ba41e
Mamat1411 <muhammadmujahid14@gmail.com>
2024-02-06 14:50:08 +0700

    Add Repository Annotation on UserRepo

--- src/main/java/com/springboot/springblogmvc/repositories/UserRepository.java
+++ src/main/java/com/springboot/springblogmvc/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.springboot.springblogmvc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springboot.springblogmvc.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     
 }
